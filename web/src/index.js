e29c7c588b680cd5ac02fbdc6cbf40de50e44ae4
Caroline Moura <54120284+CahMoura@users.noreply.github.com>
2020-01-26 21:49:53 -0300

    update

---
+++ web/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+//JSX (JavaScript + XML) O XML é a sintaxe do HTML. O REACT é importado quando o HML é usado no código
+//renderizando o App no Root ou seja está como que enjetando
